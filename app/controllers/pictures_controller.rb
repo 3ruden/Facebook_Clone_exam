203a228ba019e6b47e1b3d25454f60b823d0a063
micchan <p1r2yy9knr@gmail.com>
2020-01-16 22:59:33 +0900

    コードをリファクタリング

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,5 @@
 class PicturesController < ApplicationController
-  before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :set_picture, only: %i(show edit update destroy)
 
   def index
     @pictures = Picture.all
@@ -17,36 +17,24 @@ class PicturesController < ApplicationController
 
   def create
     @picture = current_user.pictures.build(picture_params)
-
-    respond_to do |format|
-      if @picture.save
-        format.html { redirect_to @picture, notice: '投稿しました！' }
-        format.json { render :show, status: :created, location: @picture }
-      else
-        format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.save
+      redirect_to @picture, notice: '投稿しました！'
+    else
+      render :new
     end
   end
 
   def update
-    respond_to do |format|
-      if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: '投稿を編集しました！' }
-        format.json { render :show, status: :ok, location: @picture }
-      else
-        format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
-      end
+    if @picture.update(picture_params)
+      redirect_to @picture, notice: '投稿を編集しました！'
+    else
+      render :edit
     end
   end
 
   def destroy
     @picture.destroy
-    respond_to do |format|
-      format.html { redirect_to pictures_url, notice: '投稿を削除しました！' }
-      format.json { head :no_content }
-    end
+    redirect_to pictures_url, notice: '投稿を削除しました！'
   end
 
   private
