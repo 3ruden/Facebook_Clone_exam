641646240a1b87f1aec964906a019dc833ed0b33
micchan <p1r2yy9knr@gmail.com>
2020-01-17 12:03:48 +0900

    タイムゾーンの設定を日本語にした

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,8 @@ Bundler.require(*Rails.groups)
 module FacebookCloneExam
   class Application < Rails::Application
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     I18n.config.available_locales = :ja
     I18n.default_locale = :ja
   end
