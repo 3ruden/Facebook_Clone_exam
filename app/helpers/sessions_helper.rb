a1960e8f7b11495ac4436d5ab5e94ce93eb3300d
micchan <p1r2yy9knr@gmail.com>
2020-01-17 10:49:47 +0900

    コードをリファクタリング

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,4 +6,16 @@ module SessionsHelper
   def logged_in?
     current_user.present?
   end
+
+  def authenticate_user
+    if current_user.nil?
+      redirect_to new_session_url, notice: "ログインが必要です"
+    end
+  end
+
+  def forbid_login_user
+    if current_user.present?
+      redirect_to pictures_url, notice: "すでにログインしています"
+    end
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,13 +1,2 @@
 module UsersHelper
-  def authenticate_user
-    if current_user.nil?
-      redirect_to new_session_url, notice: "ログインが必要です"
-    end
-  end
-
-  def forbid_login_user
-    if current_user.present?
-      redirect_to pictures_url, notice: "すでにログインしています"
-    end
-  end
 end
