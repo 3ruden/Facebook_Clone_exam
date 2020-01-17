000716ebc7d45014cebd1e0660094722aa6e5ebe
micchan <p1r2yy9knr@gmail.com>
2020-01-17 11:12:51 +0900

    デフォルトメッセージの日本語化

--- config/application.rb
+++ config/application.rb
@@ -7,5 +7,7 @@ Bundler.require(*Rails.groups)
 module FacebookCloneExam
   class Application < Rails::Application
     config.load_defaults 5.2
+    I18n.config.available_locales = :ja
+    I18n.default_locale = :ja
   end
 end
